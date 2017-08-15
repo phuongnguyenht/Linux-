b6de35b802b56b158e65113da4ec420f41a47dc5
phuongnguyenht <phuongntsv@gmail.com>
2017-08-15 16:13:13 +0700

    Update send_file_ftp.sh

--- send_file_ftp.sh
+++ send_file_ftp.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+DIR="/path/x"
+DEST="/path/y"
+file=`ls $DIR`
+FTPLOG=/var/log/ftplog/
+day=`/bin/date +%Y-%m-%d`
+file="ftp_$day.txt"
 
-HOST="x.x.x.x"
+HOST="x.y.z.t"
 USER="user"
-PASSWD="password"
+PASSWD="pass"
+DATE=`date +%F_%H:%M:%S`;
 
-#path to folder have file send to server ftp
-cd /path/x/
+cd $DIR
 sleep 1
 FILE=`ls -t *.txt| head -1`
 
-ftp -n $HOST << EOF
-quote USER $USER
-quote PASS $PASSWD
+ftp -n -v $HOST << END | tee /tmp/temp.txt
+user $USER $PASSWD
 put $FILE
-EOF
+END
+
+Search=$FILE
+FileToSearch="/tmp/temp.txt"
+if grep $Search $FileToSearch; then
+   echo $DATE >> $FTPLOG/$file
+   echo "$FILE send OK"
+   cat /tmp/temp.txt >> $FTPLOG/$file
+   mv $FILE $DEST
+   count=`cat $FTPLOG/$file | grep -e "bytes sent" | wc -l`
+   echo "Count File sent is:"$count >> $FTPLOG/$file
+else
+   echo "$FILE send Fail cmnr!."
+fi
+echo Author by: Phuong Nguyen
